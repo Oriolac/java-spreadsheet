1e637eb86f7443be7d1a4fdee0db2ddddd572cfc
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-24 17:19:31 +0200

    Refactor, visited in TemplateExpressionVisitor no need it

--- src/cat/udl/abbddep/expression/visitor/TemplateExpressionVisitor.java
+++ src/cat/udl/abbddep/expression/visitor/TemplateExpressionVisitor.java
@@ -10,28 +10,22 @@ import java.util.Set;
 
 public abstract class TemplateExpressionVisitor implements ExpressionVisitor{
 
-    private final Set<Expression> visited;
-
     public TemplateExpressionVisitor() {
-        this.visited = new HashSet<>();
     }
 
     @Override
     public void visit(MaybeValue value) {
-        if (!visited.add(value)) return;
         doVisit(value);
     }
 
     @Override
     public void visit(Operation op) {
-        if (!visited.add(op)) return;
         op.getFirstExpression().accept(this);
         op.getSecondExpression().accept(this);
     }
 
     @Override
     public void visit(Reference r) {
-        if (!visited.add(r)) return;
         doVisit(r);
     }
 
